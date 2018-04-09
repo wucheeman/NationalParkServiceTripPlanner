c655cff56aac40a5ed436611940937ae56e21eb0
steve27519 <30443106+steve27519@users.noreply.github.com>
2018-04-09 00:03:53 -0400

    Update nps.js
    added arrays and varibles

--- assets/javascript/nps.js
+++ assets/javascript/nps.js
@@ -2,3 +2,18 @@ $("#submitBtn").on("click", function (event) {
   console.log("hi")
   // event.preventDefault();
 });
+
+    var parkCodes = [
+    acad,npsa,arch,badl,bibe,bisc,blca,brca,cany,care,cave,chis,cong,crla,cuva,deva,dena,drto,ever,gaar,jeff,glac,glba,grca,grta,grba,grsa,grsm,gumo,hale,havo,hosp,isro,jotr,katm,kefj,seki,kova,lacl,lavo,maca,meve,mora,noca,olym,pefo,pinn,redw,romo,sagu,shen,thro,viis,voya,wica,wrst,yell,yose,zion
+]
+            var randomSelect=Math.floor(Math.random() * 61);
+ var queryQRL = "https://api.giphy.com/v1/gifs/search?parkCodes" + randomSelect + "&api_key=oc4HHNynCW6opSic49NXu4XgDx2poMUu";
+        console.log(queryQRL);
+
+
+        $.ajax({
+            url: queryQRL,
+            method: "GET"
+        }).done(function (response) {
+            var random = $('randomSelect')
+        }
