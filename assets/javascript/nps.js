ccfab70d889570bf57fb5ebe767cf787e5f75d4c
steve27519 <30443106+steve27519@users.noreply.github.com>
2018-04-09 22:57:22 -0400

    Update nps.js
    corrected query API and variable, added parks array

--- assets/javascript/nps.js
+++ assets/javascript/nps.js
@@ -6,9 +6,74 @@ $("#submitBtn").on("click", function (event) {
     var parkCodes = [
     acad,npsa,arch,badl,bibe,bisc,blca,brca,cany,care,cave,chis,cong,crla,cuva,deva,dena,drto,ever,gaar,jeff,glac,glba,grca,grta,grba,grsa,grsm,gumo,hale,havo,hosp,isro,jotr,katm,kefj,seki,kova,lacl,lavo,maca,meve,mora,noca,olym,pefo,pinn,redw,romo,sagu,shen,thro,viis,voya,wica,wrst,yell,yose,zion
 ]
+    var parks = [ "Acadia",
+              "American Samoa",
+              Arches,
+              Badlands,
+              Big Bend,
+              Biscayne,
+              Black Canyon of the Gunnison,
+              Bryce Canyon,
+              Canyonlands,
+              Capitol Reef, 
+              Carlsbad Caverns,
+              Channel Islands,
+              Congaree,
+              Crater Lake,
+              Cuyahoga Valley,
+              Death Valley,
+              Denali,
+              Dry Tortugas,
+              Everglades,
+              Gates of the Arctic,
+              Gateway Arch,
+              Glacier,
+              Glacier Bay,
+              Grand Canyon,
+              Grand Teton,
+              Great Basin,
+              Great Sand Dunes,
+              Great Smoky Mountains,
+              <Guadalupe Mountains,
+              Haleakalā,
+              Hawaiʻi Volcanoes,
+              Hot Springs,
+              <Isle Royale,
+              Joshua Tree,
+              Katmai,
+              Kenai Fjords,
+              Kings Canyon,
+              Kobuk Valley,
+              Lake Clark,
+              Lassen Volcanic,
+              Mammoth Cave,
+              Mesa Verde,
+              Mount Rainier
+              North Cascades
+              Olympic,
+              Petrified Forest,
+              Pinnacles,
+              Redwood,
+              Rocky Mountain,
+              Saguaro,
+              Sequoia,
+              Shenandoah,
+              Theodore Roosevelt,
+              Virgin Islands,
+              Voyageurs,
+              Wind Cave,
+              Wrangell–St. Elias,
+              Yellowstone,
+              Yosemite,
+              Zion,
+    ]
+
+    
             var randomSelect=Math.floor(Math.random() * 61);
- var queryQRL = "https://api.giphy.com/v1/gifs/search?parkCodes" + randomSelect + "&api_key=oc4HHNynCW6opSic49NXu4XgDx2poMUu";
+            var selectedCode=parkCodes[randomNumber]
+            var queryQRL = "https://developer.nps.gov/api/v1/parks?parkCode" + randomNumber + "&api_key=oc4HHNynCW6opSic49NXu4XgDx2poMUu";
         console.log(queryQRL);
+        
 
 
         $.ajax({
