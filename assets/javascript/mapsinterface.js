adcd1614124e4fe1c4341b7f6455480193e35ee7
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-04-10 20:13:13 -0400

    maps stuff

--- assets/javascript/mapsinterface.js
+++ assets/javascript/mapsinterface.js
@@ -1,9 +1,11 @@
+
+
 $("#submitBtn").on("click", function (event) {
     event.preventDefault();
-    var place = $('#find').val().trim();
+    var place = $('#find').val().trim()+'national park';
     console.log(place);
-   
-    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + place+'nationalpark' + "&sensor=false";
+    $('#map').append(' <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCESN4bg_XY8N82CwU7ssef7snFR53K1rY&q=' + place + '" allowfullscreen></iframe >')
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + place+ "&sensor=false";
 
     $.ajax({
         url: queryURL,
@@ -14,3 +16,4 @@ $("#submitBtn").on("click", function (event) {
 
     });
 });
+
