371dfd9aa5ac1cf6a4121bde7b01bef217c3bbe5
Mark Hainline <wucheeman@gmail.com>
2018-04-11 07:05:43 -0400

    added signatures for a additional methods that team may require

--- assets/javascript/dbif.js
+++ assets/javascript/dbif.js
@@ -59,6 +59,35 @@ const dbInterface = {
     // processUserEmail
     // retrieve password using email as key
     // if good return true, else return false
+  },
+  // methods below will be built if they are required and time permits 
+  createNewPlan: function(userEmail, newPlan) {
+    // TODO
+    // stores a newly created plan in the database
+    // plan is a JSON object. Only one plan stored at a time
+  },
+  updatePlan: function(userEmail, plan) {
+    // TODO
+    // overwrites existly plan with updated content
+    // plan is a JSON object
+  },
+  deletePlan: function(userEmail) {
+    // TODO
+    // returns 'success' if successful or 'failed' if not
+  },
+  createNewMemory: function(userEmail, newMemory) {
+    // TODO if required
+    // stores a newly created JSON object in the database to save memories of trip
+    // Only one 'memory' is stored at a time
+  },
+  updateMemory: function(userEmail, memory) {
+    // TODO if required
+    // overwrites existly memory with updated content
+    // memory is a JSON object
+  },
+  deleteMemory: function(userEmail) {
+    // TODO if required
+    // returns 'success' if successful or 'failed' if not
   }
 }
 
