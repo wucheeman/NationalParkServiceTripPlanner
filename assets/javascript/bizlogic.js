7c77d4d661d317b2ae1e850ae1513fc7e256992f
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-04-12 00:01:26 -0400

    facebook sdk to bzjs. facebook share button added for nps.gov

--- assets/javascript/bizlogic.js
+++ assets/javascript/bizlogic.js
@@ -26,4 +26,13 @@ const captureProfileData = () => {
   })
 }
 
+
+function LoadFbSDK (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+ }   (document, 'script', 'facebook-jssdk');
+
 });
